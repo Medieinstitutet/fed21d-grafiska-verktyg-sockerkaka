e4796ec56e47b2f19556cf7dfd3e256027cf1c3c
tovehydmark <tovehydmark@gmail.com>
2022-09-22 18:23:22 +0200

    style prices + script for hamburger menu

---
+++ script.js
@@ -0,0 +1,20 @@
+// <script>
+
+//     {/* let hamburgerBoolean = false; */}
+
+// {/* function toggleHamburger() {
+//   hamburgerBoolean = !hamburgerBoolean;
+//   console.log(hamburgerBoolean);
+// } */}
+
+// {/* console.log(hamburgerBoolean); */}
+
+// const hamburger = document.querySelector('#hamburgerMenu');
+
+// hamburger.addEventListener('click',  ()=> {
+//   console.log('klick');
+//   toggleHamburger();
+//   console.log(hamburgerBoolean);
+// })
+
+// </script>
