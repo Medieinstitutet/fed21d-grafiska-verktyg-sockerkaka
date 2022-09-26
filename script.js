40631a421982b0e343fd8bd95e5f01c757d527db
tovehydmark <tovehydmark@gmail.com>
2022-09-26 13:26:17 +0200

    animate quote box

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('menu-open');
   frontpageElementsToHide.classList.toggle('toggle-quote-card-section');
 });
+
 acceptCookiesBtn.addEventListener('click', () => {
   document.getElementsByClassName('cookie-container')[0].style.display = 'none';
+});
+
+//Animations
+const card = document.getElementById('quote-card-animation');
+card.addEventListener('mouseover', function () {
+  gsap.to('#quote-card-animation', {
+    y: -10,
+    duration: 0.2,
+    yoyo: true,
+    repeat: 1,
+  });
 });
